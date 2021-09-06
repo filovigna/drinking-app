76209f4a933ef9b6a47d50d3cb31434dc1028b5c
filovigna <filippo.vignali@gmail.com>
2021-09-06 22:20:20 +0200

    New Project Directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'drinkin-app',
+  appName: 'drinking-app',
   webDir: 'dist',
   bundledWebRuntime: false
 };
