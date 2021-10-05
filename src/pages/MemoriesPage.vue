bf380fe1783df07b9ce2a14ac839c756b1041c12
filovigna <filippo.vignali@gmail.com>
2021-10-05 23:05:42 +0200

    project updated

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+      <ion-title>The Memories Page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>Max</ion-item>
+        <ion-item>George</ion-item>
+        <ion-item>Julie</ion-item>
+        <ion-item>Filippo</ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+
+<script>
+import { IonPage, IonHeader, IonTitle, IonContent, IonToolbar, IonList, IonItem } from "@ionic/vue";
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+    IonList,
+    IonItem
+  },
+};
+</script>
